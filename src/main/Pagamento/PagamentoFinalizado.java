795b4af2dc6231920b2fb3b0f29287393728854c
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2025-03-04 15:07:31 -0300

    Criação da classe filha PagamentoFinalizado

---
+++ src/main/Pagamento/PagamentoFinalizado.java
@@ -0,0 +1,33 @@
+package Pagamento;
+
+import Notificacao.Notificacao;
+import cliente.Cliente;
+import enums.StatusPedido;
+import pedido.Pedido;
+
+public class PagamentoFinalizado {
+
+    Cliente cliente;
+
+
+    public static void finalizarPagamento(Cliente cliente, Pedido pedido) {
+
+        Notificacao notificacao = new Notificacao(cliente, pedido);
+
+        if (pedido.getStatusPedido() == StatusPedido.FINALIZADO) {
+            System.out.println("O seu pedido já foi finalizado.");
+            return;
+        }
+
+        if (pedido.getStatusPedido() != StatusPedido.PAGO) {
+            System.out.println("O status do pedido não está em 'pago', realize o pagamento para poder prosseguir.");
+            return;
+        }
+        pedido.setStatusPedido(StatusPedido.FINALIZADO);
+        System.out.println("Compra realizada com sucesso!");
+
+        notificacao.enviarNotificacoes(pedido);
+
+    }
+
+}
